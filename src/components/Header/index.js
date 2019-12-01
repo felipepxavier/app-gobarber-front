6b63fe0a40265f2c3fef24ae36ec4d11018261b7
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2019-11-30 21:17:20 -0300

    criando componente Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo_roxo.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Felipe Pedroso</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/51/abott@adorable.png"
+              alt="Foto perfil"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
