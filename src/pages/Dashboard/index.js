d6203e1ea36a496f2ebe92de2872a4111df873fb
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2019-12-02 08:12:29 -0300

    usando date-fns

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    /**reduzindo em 1 dia o valor do 'date' */
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    /**aumentando em 1 dia o valor do 'date' */
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>31 de Maio</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
